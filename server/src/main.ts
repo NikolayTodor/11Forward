90413ef57de7d444d9dc09ae384578077d6d1558
Nikolay <nikolaytodor84@gmail.com>
2019-11-14 20:24:16 +0200

    importing ConfigService and enabling cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  app.enableCors();
+
+  await app.listen(app.get(ConfigService).port);
 }
 bootstrap();
