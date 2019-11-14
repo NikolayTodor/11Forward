22a7098f1defa987991b330b70ffd0231fc0d6a6
Nikolay <nikolaytodor84@gmail.com>
2019-11-14 20:38:15 +0200

    fixing lint failures / db tested - success

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors();
 
   await app.listen(app.get(ConfigService).port);
