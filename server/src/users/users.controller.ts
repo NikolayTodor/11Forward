b3a2d25e05b571a28d82ebd34b96c055c12047d8
Nikolay <nikolaytodor84@gmail.com>
2019-11-15 17:11:58 +0200

    implementing usersController + create User method

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Post, HttpCode, HttpStatus, UsePipes, ValidationPipe, Body } from "@nestjs/common";
+import { CreateUserDTO } from "src/models/users/create-user.dto";
+import { UsersDataService } from "./users-data.service";
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private readonly usersService: UsersDataService) {
+
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @UsePipes(new ValidationPipe({whitelist: true, transform: true}))
+
+    public async addNewUser(@Body() newUser: CreateUserDTO) {
+
+        return await this.usersService.createUser(newUser);
+    }
+
+}
