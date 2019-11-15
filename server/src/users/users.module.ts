c44e43f0b3cb59a3f2805ff065ba7d45b900163e
Nikolay <nikolaytodor84@gmail.com>
2019-11-15 17:30:19 +0200

    importing controller and service

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,8 +1,16 @@
+import { UsersDataService } from './users-data.service';
+import { UsersController } from './users.controller';
 import { User } from './../data/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])]
+    imports: [TypeOrmModule.forFeature([User])],
+
+    controllers: [UsersController],
+
+    providers: [UsersDataService],
+
+    exports: [UsersDataService]
 })
 export class UsersModule {}
