8807670db172d5f0a3586c919e77d9300cf1203b
Nikolay <nikolaytodor84@gmail.com>
2019-11-18 20:07:58 +0200

    providing api-system-error filter

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_FILTER } from '@nestjs/core';
+import { ApiSystemErrorFilter } from './common/filters/api-error.filter';
 
 @Module({
   imports: [DatabaseModule, UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_FILTER,
+      useClass: ApiSystemErrorFilter,
+    }],
 })
 export class AppModule {}
