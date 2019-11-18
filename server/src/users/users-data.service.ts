b0b1296996e8eaddbc025ca97dc01998726eb94e
Nikolay <nikolaytodor84@gmail.com>
2019-11-18 20:00:33 +0200

    find user by name / validate password methods

--- server/src/users/users-data.service.ts
+++ server/src/users/users-data.service.ts
@@ -1,9 +1,12 @@
+import { AuthUserDTO } from './../models/users/auth-user.dto';
+import { ShowUserDTO } from './../models/users/show-user.dto';
 import { User } from './../data/entities/user.entity';
 import { CreateUserDTO } from './../models/users/create-user.dto';
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import * as bcrypt from 'bcrypt';
+import { plainToClass } from 'class-transformer';
 
 
 @Injectable()
@@ -14,6 +17,25 @@ export class UsersDataService {
     ) {
 
     }
+
+    public async findUserByUsername(username: string): Promise<ShowUserDTO> {
+        const foundUser: User = await this.userRepo.findOne({
+          username,
+          isDeleted: false,
+        });
+
+        return plainToClass(ShowUserDTO, foundUser, {
+          excludeExtraneousValues: true,
+        });
+      }
+
+      public async validateUserPassword(user: AuthUserDTO): Promise<boolean> {
+        const userEntity: User = await this.userRepo.findOne({
+          username: user.username,
+        });
+
+        return await bcrypt.compare(user.password, userEntity.password);
+      }
 
     public async createUser(
         userToCreate: CreateUserDTO
