d2002d1b5b92be44dda760a877fff7b4304c5a17
peteratan <peteratan@gmail.com>
2019-11-18 22:27:58 +0200

    #2 login and ugly home & header-navbar added :D

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,11 +9,15 @@ import { SharedModule } from './shared/shared.module';
 import { UsersModule } from './users/users.module';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     CoreModule,
