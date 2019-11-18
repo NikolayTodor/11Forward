62068fc3d018b3954011262f526cbb14cf657499
Nikolay <nikolaytodor84@gmail.com>
2019-11-18 20:03:27 +0200

    implementing jwt strategy

--- server/src/auth/strategy/jwt.strategy.ts
+++ server/src/auth/strategy/jwt.strategy.ts
@@ -1,9 +1,30 @@
+import { ShowUserDTO } from './../../models/users/show-user.dto';
+import { UsersDataService } from './../../users/users-data.service';
+import { ConfigService } from './../../config/config.service';
 import { PassportStrategy } from "@nestjs/passport";
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ExtractJwt, Strategy } from 'passport-jwt';
-
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
 
+    constructor(private readonly userDataService: UsersDataService,
+        configservice: ConfigService) {
+        super({
+          jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+          secretOrKey: configservice.jwtSecret,
+          ignoreExpiration: false
+        });
+      }
+
+      public async validate(payload: ShowUserDTO): Promise<ShowUserDTO> {
+        const user = await this.userDataService.findUserByUsername(
+          payload.username,
+        );
+
+        if (!user) {
+          throw new UnauthorizedException();
+        }
+        return user;
+      }
 }
