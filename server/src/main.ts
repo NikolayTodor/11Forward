d44c27f9cab0a5900d0e8e902a52c8bb1fcfc858
Nikolay <nikolaytodor84@gmail.com>
2019-11-18 17:28:48 +0200

    setting swagger options for users crud

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('User management')
+    .setDescription('User management application')
+    .setVersion('1.0')
+    .addTag('user-management')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(app.get(ConfigService).port);
 }
