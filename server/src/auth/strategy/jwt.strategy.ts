c93b38ac65135ad43feceed9abc5889ae6f7a12f
Nikolay <nikolaytodor84@gmail.com>
2019-11-18 18:27:42 +0200

    jwt strategy stub

---
+++ server/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,9 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable } from "@nestjs/common";
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+
+}
