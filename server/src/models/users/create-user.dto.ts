10ba7747f1778944a702677a151d2db0feb2f9fc
Nikolay <nikolaytodor84@gmail.com>
2019-11-18 18:06:33 +0200

    Applying swagger

--- server/src/models/users/create-user.dto.ts
+++ server/src/models/users/create-user.dto.ts
@@ -1,16 +1,20 @@
 import {IsString, IsNotEmpty, Length, IsEmail} from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class CreateUserDTO {
 
+    @ApiModelProperty()
     @IsString()
     @IsNotEmpty()
     @Length(4, 15)
     public username: string;
 
+    @ApiModelProperty()
     @IsEmail()
     @IsNotEmpty()
     public email: string;
 
+    @ApiModelProperty()
     @IsNotEmpty()
     @IsString()
     @Length(4, 15)
