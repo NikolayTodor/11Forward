5bd541310747fed745345d46afe1090ca0611733
Nikolay <nikolaytodor84@gmail.com>
2019-11-18 20:07:26 +0200

    applying auth controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { AuthUserDTO } from './../models/users/auth-user.dto';
+import { Controller, Post, UsePipes, ValidationPipe, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+
+@Controller('session')
+export class AuthController {
+    constructor(private readonly authservice: AuthService) {}
+
+    @Post()
+    @UsePipes(new ValidationPipe({whitelist: true, transform: true}))
+    public async logIn(@Body() body: AuthUserDTO ) {
+        return await this.authservice.login(body);
+    }
+
+}
