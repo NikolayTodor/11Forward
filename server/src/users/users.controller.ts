d32a251cbc35dc62a5bd1b3dff22b3b63b52bcea
Nikolay <nikolaytodor84@gmail.com>
2019-11-23 12:44:19 +0200

    test method to display list of followers/following

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,7 +1,13 @@
+import { TransformInterceptor } from './../transformer/interceptors/transform.interceptor';
+import { UserFollowInfoDTO } from './../models/users/user-follow-info.dto';
+import { ShowUserDTO } from './../models/users/show-user.dto';
+import { FollowActionType } from './../common/enums/follow-action-type';
+import { userDecorator } from './../common/decorators/user.decorator';
 import { CreateUserDTO } from './../models/users/create-user.dto';
-import { Controller, Post, HttpCode, HttpStatus, UsePipes, ValidationPipe, Body } from "@nestjs/common";
+import { Controller, Post, HttpCode, HttpStatus, UsePipes, ValidationPipe, Body, Patch, UseGuards, Param, ParseIntPipe, Get, UseInterceptors } from "@nestjs/common";
 import { UsersDataService } from "./users-data.service";
 import { ApiUseTags, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 @ApiUseTags('Users Controller')
@@ -13,6 +19,17 @@ export class UsersController {
 
     }
 
+    // ----- test method for showing user followers and following ------ 
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get(':name')
+    @HttpCode(HttpStatus.OK)
+    @UseInterceptors(new TransformInterceptor(UserFollowInfoDTO))
+    public async showUserFollowInfo(@Param('name') userName: string ) {
+        return await this.usersService.showFollow(userName);
+    }
+    //
+
     @Post()
     @HttpCode(HttpStatus.CREATED)
     @UsePipes(new ValidationPipe({whitelist: true, transform: true}))
@@ -22,4 +39,18 @@ export class UsersController {
         return await this.usersService.createUser(newUser);
     }
 
+    @UseGuards(AuthGuard('jwt'))
+    @Patch(':name')
+    @HttpCode(HttpStatus.OK)
+    @UsePipes(new ValidationPipe({whitelist: true, transform: true}))
+    public async followUnfollow (@userDecorator('user') user: ShowUserDTO,
+                        @Body() body: { action: FollowActionType },
+                        @Param('name') followUserName: string
+                        ) {
+                            if ( body.action === FollowActionType.Follow ) {
+                                return await this.usersService.followUser(user.id, followUserName);
+                            } else{
+                                return await this.usersService.unfollowUser(user.id, followUserName);
+                            }
+                        }
 }
