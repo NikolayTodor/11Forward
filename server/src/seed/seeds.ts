ae549bcab441cf8835d1e5fd6f7e5d2ca374bf68
Nikolay <nikolaytodor84@gmail.com>
2019-11-23 12:20:00 +0200

    Promise.resolve for lazy-loaded relations

--- server/src/seed/seeds.ts
+++ server/src/seed/seeds.ts
@@ -11,18 +11,24 @@ const main = async () => {
     defUser.username = 'Niki';
     defUser.password = await bcrypt.hash('123456', 10);
     defUser.email = "niki@abv.bg";
+    defUser.followers = Promise.resolve([]);
+    defUser.following = Promise.resolve([]);
     await userRepo.save(defUser);
 
     const defUserTwo = new User();
     defUserTwo.username = 'Alpha';
     defUserTwo.password = await bcrypt.hash('123456', 10);
     defUserTwo.email = "alpha@abv.bg";
+    defUserTwo.followers = Promise.resolve([]);
+    defUserTwo.following = Promise.resolve([]);
     await userRepo.save(defUserTwo);
 
     const defUserThree = new User();
     defUserThree.username = 'Beta';
     defUserThree.password = await bcrypt.hash('123456', 10);
     defUserThree.email = "beta@abv.bg";
+    defUserThree.followers = Promise.resolve([]);
+    defUserThree.following = Promise.resolve([]);
     await userRepo.save(defUserThree);
 
     await connection.close();
