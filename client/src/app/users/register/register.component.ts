357e7692ec6eafe76100f7477339987e93a44646
peteratan <peteratan@gmail.com>
2019-11-24 13:52:25 +0200

    #2 confirm password field added

--- client/src/app/users/register/register.component.ts
+++ client/src/app/users/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidatorFn, ValidationErrors, FormControl, FormGroupDirective } from '@angular/forms';
 import { UserRegisterDTO } from 'src/app/models/user-register.dto';
 import { NotificationService } from 'src/app/core/services/notification.service';
 
@@ -12,17 +12,31 @@ import { NotificationService } from 'src/app/core/services/notification.service'
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+
+  public constructor(
+    private readonly authService: AuthService,
+    private readonly notification: NotificationService,
+    private readonly router: Router,
+    private readonly formBuilder: FormBuilder) { }
   public registerForm: FormGroup;
 
   public username: string;
   public email: string;
   public password: string;
+  public password2: string;
 
-  public constructor(
-    private readonly authService: AuthService,
-    private readonly notification: NotificationService,
-    private readonly router: Router ,
-    private readonly formBuilder: FormBuilder) { }
+  public confirmErrorMatcher = {
+    isErrorState: (control: FormControl, registerForm: FormGroupDirective): boolean => {
+      const controlInvalid = control.touched && control.invalid;
+      const formInvalid = control.touched && this.registerForm.get('password').touched && this.registerForm.invalid;
+      return controlInvalid || formInvalid;
+    }
+  };
+
+  public passwordMatchValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
+    return formGroup.get('password').value === formGroup.get('password2').value ?
+      null : { passwordMismatch: true };
+  }
 
 
     public ngOnInit() {
@@ -35,21 +49,23 @@ export class RegisterComponent implements OnInit {
           '',
           [Validators.required, Validators.minLength(6), Validators.maxLength(50)]
         ],
+        password2: ['', [Validators.required]],
         email: [
           '',
           [Validators.required, Validators.email]
-        ],
-      });
+        ]
+      }, { validators: this.passwordMatchValidator });
     }
 
   public register(user: UserRegisterDTO) {
     this.authService.register(user).subscribe(
       () => {
       this.notification.success('Registered successfully!!!');
-      this.router.navigate(['home']);
+      this.router.navigate(['users/login']);
     },
-    () => {
-      this.notification.error('Registration failed!');
+    (data) => {
+      console.log(data);
+      this.notification.error(data);
     }
     );
   }
