cc1ff7389b1bc8eb1f181467183ddfc7001694f9
Nikolay <nikolaytodor84@gmail.com>
2019-11-27 10:16:34 +0200

    importing shared service for image upload

--- client/src/app/posts/create-post/create-post.component.ts
+++ client/src/app/posts/create-post/create-post.component.ts
@@ -1,3 +1,4 @@
+import { SharedService } from './../../shared/services/shared.service';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { CreatePostDTO } from '../../models/create-post.dto';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -9,7 +10,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CreatePostComponent implements OnInit {
   public createPostForm: FormGroup;
-  public constructor(private readonly formBuilder: FormBuilder) { }
+  public createPostUrl: string;
+
+  public constructor(private readonly formBuilder: FormBuilder,
+                     private readonly sharedService: SharedService) { }
 
   @Output() public readonly toCreatePost: EventEmitter<CreatePostDTO> = new EventEmitter();
 
@@ -17,14 +21,27 @@ export class CreatePostComponent implements OnInit {
     this.createPostForm = this.formBuilder.group({
       title: ['', Validators.required],
       content: ['', Validators.required],
-      imageURL: ['', Validators.required],
       isPrivate: ['', Validators.required]
     });
   }
 
+
+
+  onFileChange(event) {
+
+  const file = event.target.files[0];
+  if (file.type !== 'image/jpeg' && file.type !== 'image/png' ) {
+    console.log('You are trying to upload invalid file!');
+  }
+
+  this.sharedService.postImgAndGetUrl(file).toPromise().then((response: any) => this.createPostUrl = response.data.link);
+  }
+
+
   public createPost(post): void {
     const postToCreate: CreatePostDTO = {
       ...post,
+      imageURL: this.createPostUrl,
       isPrivate: post.isPrivate === 'Private' ? true : false
     };
     this.toCreatePost.emit(postToCreate);
