69a34a08fd0671a168a442d0d5215e2cf0c44a74
Nikolay <nikolaytodor84@gmail.com>
2019-11-28 17:17:06 +0000

    Expose decorator unnecessary for dto

--- server/src/models/users/show-user-profile.dto.ts
+++ server/src/models/users/show-user-profile.dto.ts
@@ -1,20 +1,13 @@
-import { Expose } from 'class-transformer';
-
 export class ShowUserProfileDTO {
 
-    @Expose()
     public id: string;
 
-    @Expose()
     public username: string;
 
-    @Expose()
     public email: string;
 
-    @Expose()
     public followersCount: number;
 
-    @Expose()
     public followingCount: number;
 
 }
