b6f76705b2d90baff9d7a1931829fb3ace72b944
Nikolay <nikolaytodor84@gmail.com>
2019-11-29 18:34:03 +0200

    adding auth-guard to profile routing

--- client/src/app/users/users-routing.module.ts
+++ client/src/app/users/users-routing.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const userRoutes: Routes = [
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
-  { path: ':id', component: ProfileComponent}
+  { path: ':id', component: ProfileComponent, canActivate: [AuthGuard]}
   // { path: 'all', component: UsersAllComponent , pathMatch: 'full'},
   // { path: ':id', component: UpdateUserComponent },
 ];
