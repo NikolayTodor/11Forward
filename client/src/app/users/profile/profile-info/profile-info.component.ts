4f867fe3b445a8c95c97fa36f4b855b79bd4f340
Nikolay <nikolaytodor84@gmail.com>
2019-11-30 16:36:19 +0200

    renamig variable 'userInfo'>'profileInfo'

--- client/src/app/users/profile/profile-info/profile-info.component.ts
+++ client/src/app/users/profile/profile-info/profile-info.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProfileInfoComponent implements OnInit {
 
-  @Input() userInfo: LoggedUserDTO;
+  @Input() profile: LoggedUserDTO;
   constructor() { }
 
   ngOnInit() {
