1fd3c526aed02cff1ad20a9a0ad4bf0997db7493
Nikolay <nikolaytodor84@gmail.com>
2019-11-30 16:19:54 +0200

    adding auth-guard to profile routing

---
+++ client/src/app/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
+import { NotificationService } from '../core/services/notification.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+    private readonly notificationService: NotificationService
+  ) {}
+
+  public canActivate(): boolean {
+    if (!this.authService.getLoggedUserData()) {
+      this.notificationService.error(
+        `You must be logged in in order to see this page!`
+      );
+      this.router.navigate(['users/login']);
+
+      return false;
+    }
+
+    return true;
+  }
+}
