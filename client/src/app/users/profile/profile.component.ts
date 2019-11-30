362d54060a78e4c390a726a6705eca471dbf5fa8
Nikolay <nikolaytodor84@gmail.com>
2019-11-30 16:23:35 +0200

    smart profile component for user state

--- client/src/app/users/profile/profile.component.ts
+++ client/src/app/users/profile/profile.component.ts
@@ -1,6 +1,8 @@
+
 import { LoggedUserDTO } from './../../models/logged-user.dto';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -12,12 +14,19 @@ export class ProfileComponent implements OnInit {
   public loggedUser: LoggedUserDTO;
   public subscription: Subscription;
 
-  constructor() { }
+  constructor(private readonly authService: AuthService
+              ) { }
 
   ngOnInit() {
 
-    
+    this.subscription = this.authService.loggedUserData$.subscribe(
+      (loggedUser: LoggedUserDTO) => {
+        this.loggedUser = loggedUser;
+        console.log(this.loggedUser);
+      });
+
+    }
+
 
   }
 
-}
