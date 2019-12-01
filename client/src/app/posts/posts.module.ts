972555f9657e51a2b40daecfa0c7b85b23e248e2
Nikolay <nikolaytodor84@gmail.com>
2019-12-01 15:24:59 +0200

    single post made availabe in shared

--- client/src/app/posts/posts.module.ts
+++ client/src/app/posts/posts.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AllPostsComponent } from './all-posts/all-posts.component';
 import { PostsService } from './posts.service';
-import { SinglePostInListComponent } from './single-post-in-list/single-post-in-list.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 
 @NgModule({
-  declarations: [AllPostsComponent, SinglePostInListComponent, CreatePostComponent],
+  declarations: [AllPostsComponent, CreatePostComponent],
   imports: [
     CommonModule,
     SharedModule
