d2892d43e183a8b686005cf69f2f596118c9a28e
Nikolay <nikolaytodor84@gmail.com>
2019-12-01 15:22:43 +0200

    profile getting the posts and sending to dummy

--- client/src/app/users/profile/profile.component.ts
+++ client/src/app/users/profile/profile.component.ts
@@ -1,10 +1,10 @@
 import { ShowPostDTO } from './../../models/show-post.dto';
-
 import { LoggedUserDTO } from './../../models/logged-user.dto';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../core/services/auth.service';
 import { PostsService } from '../../posts/posts.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -16,9 +16,11 @@ export class ProfileComponent implements OnInit {
   public loggedUser: LoggedUserDTO;
   public subscription: Subscription;
   public profilePosts: ShowPostDTO[];
+  public profileId: string;
 
   constructor(private readonly authService: AuthService,
-    private readonly postsService: PostsService
+              private readonly postsService: PostsService,
+              private readonly activatedRoute: ActivatedRoute,
               ) { }
 
   ngOnInit() {
@@ -26,6 +28,11 @@ export class ProfileComponent implements OnInit {
     this.subscription = this.authService.loggedUserData$.subscribe(
       (loggedUser: LoggedUserDTO) => {
         this.loggedUser = loggedUser;
+      });
+
+    this.postsService.getUserPosts(this.activatedRoute.snapshot.params.id)
+      .subscribe((data: ShowPostDTO[]) => {
+        this.profilePosts = data;
       });
     }
   }
