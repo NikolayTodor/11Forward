6d24d48cfd982f08d9657fcc2637985dfdb8c01c
Nikolay <nikolaytodor84@gmail.com>
2019-12-03 15:24:37 +0200

    stub component for followers

---
+++ client/src/app/users/profile/proile-followers/proile-followers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proile-followers',
+  templateUrl: './proile-followers.component.html',
+  styleUrls: ['./proile-followers.component.scss']
+})
+export class ProileFollowersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
