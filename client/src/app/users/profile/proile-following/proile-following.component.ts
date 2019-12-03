346e40c7b768eb0cafdf81fea0ac46f8a2b12c0c
Nikolay <nikolaytodor84@gmail.com>
2019-12-03 15:23:51 +0200

    stub profile for following users created

---
+++ client/src/app/users/profile/proile-following/proile-following.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proile-following',
+  templateUrl: './proile-following.component.html',
+  styleUrls: ['./proile-following.component.scss']
+})
+export class ProileFollowingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
