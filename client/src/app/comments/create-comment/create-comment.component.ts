41a6d2e5cb65afdbb6b4e76d070b5e16c882b13f
peteratan <peteratan@gmail.com>
2019-12-03 13:06:50 +0200

    creating comments now available

---
+++ client/src/app/comments/create-comment/create-comment.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CreateCommentDTO } from 'src/app/models/create-comment.dto';
+import { CreatePostDTO } from 'src/app/models/create-post.dto';
+
+@Component({
+  selector: 'app-create-comment',
+  templateUrl: './create-comment.component.html',
+  styleUrls: ['./create-comment.component.scss']
+})
+export class CreateCommentComponent implements OnInit {
+
+  public createCommentForm: FormGroup;
+  public constructor(private readonly formBuilder: FormBuilder) { }
+
+  @Output() public readonly toCreateComment: EventEmitter<CreateCommentDTO> = new EventEmitter();
+
+  ngOnInit() {
+    this.createCommentForm = this.formBuilder.group({
+      content: ['', Validators.required]
+    });
+  }
+
+  public createComment(comment: CreateCommentDTO): void {
+    this.toCreateComment.emit(comment);
+  }
+
+}
