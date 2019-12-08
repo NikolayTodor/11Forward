b16bf9dd6838d0c620a28a934ac8ee958f72249c
Nikolay <nikolaytodor84@gmail.com>
2019-12-08 10:20:03 +0200

    removing create-post from posts module

--- client/src/app/posts/posts.module.ts
+++ client/src/app/posts/posts.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AllPostsComponent } from './all-posts/all-posts.component';
-import { CreatePostComponent } from './create-post/create-post.component';
 import { SinglePostCommentsComponent } from './single-post-comments/single-post-comments.component';
 import { CommentsModule } from '../comments/comments.module';
+
 
 @NgModule({
   declarations: [
     AllPostsComponent,
-    CreatePostComponent,
     SinglePostCommentsComponent],
   imports: [
     CommonModule,
