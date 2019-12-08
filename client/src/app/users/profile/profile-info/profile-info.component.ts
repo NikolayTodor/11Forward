ea9491ed35f24d2dc3ec075afa7efd5af5ea545b
Nikolay <nikolaytodor84@gmail.com>
2019-12-08 10:19:39 +0200

    implementing modal post creation at profile info

--- client/src/app/users/profile/profile-info/profile-info.component.ts
+++ client/src/app/users/profile/profile-info/profile-info.component.ts
@@ -1,5 +1,11 @@
+import { NotificationService } from './../../../core/services/notification.service';
+import { MatDialog } from '@angular/material';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ShowUserProfileDTO } from '../../../models/user-profile.dto';
+import { ShowPostDTO } from '../../../models/show-post.dto';
+import { CreatePostDTO } from '../../../models/create-post.dto';
+import { PostsService } from '../../../core/services/posts.service';
+import { CreatePostComponent } from '../../../shared/components/create-post/create-post.component';
 
 @Component({
   selector: 'app-profile-info',
@@ -9,11 +15,37 @@ import { ShowUserProfileDTO } from '../../../models/user-profile.dto';
 export class ProfileInfoComponent implements OnInit {
 
   @Input() profile: ShowUserProfileDTO;
-  constructor() { }
+  constructor(
+    private readonly dialog: MatDialog,
+    private readonly notificationService: NotificationService,
+    private readonly postsService: PostsService,
+
+  ) { }
 
   @Output() followUnfollow: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   ngOnInit() {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CreatePostComponent, {
+      width: '60%',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.createPost(result.data);
+      }
+    });
+  }
+
+  public createPost(post: CreatePostDTO): void {
+
+    this.postsService.createPost(post).subscribe(
+      (postCreated: ShowPostDTO) => {
+        this.notificationService.success(`Post created!`);
+      },
+      () => this.notificationService.error(`Oops! Something went wrong!`));
   }
 
   onClickFollowUnfollow(change: boolean) {
