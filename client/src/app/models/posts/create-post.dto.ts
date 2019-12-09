d147c98c6a3449a8810304f1de1d3f81ec8e8753
Nikolay <nikolaytodor84@gmail.com>
2019-12-09 21:51:56 +0200

    removed comment

--- client/src/app/models/posts/create-post.dto.ts
+++ client/src/app/models/posts/create-post.dto.ts
@@ -6,9 +6,3 @@ export class CreatePostDTO {
     public base?: string;
 }
 
-// The DTO is changed since we
-// use image cropper to produce a base64 string
-// for the cropped image and send it to the backend
-// At present this technique is suboptimal, since
-// it forces us to change this property from base64 string
-// to url string in order to create Post. Refactoring needed!!!
