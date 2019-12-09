3dfa6f21a41717c40f761c7a2a457fc82ecb406e
Nikolay <nikolaytodor84@gmail.com>
2019-12-09 12:31:03 +0200

    implemented add and remove post in user profile

--- client/src/app/users/profile/profile-gallery/profile-gallery.component.ts
+++ client/src/app/users/profile/profile-gallery/profile-gallery.component.ts
@@ -1,15 +1,19 @@
+import { NotificationService } from './../../../core/services/notification.service';
+import { GalleryRefreshService } from './gallery-refresh.service';
 import { ShowPostDTO } from '../../../models/posts/show-post.dto';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PostsService } from 'src/app/core/services/posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile-gallery',
   templateUrl: './profile-gallery.component.html',
   styleUrls: ['./profile-gallery.component.scss']
 })
-export class ProfileGalleryComponent implements OnInit {
+export class ProfileGalleryComponent implements OnInit, OnDestroy {
 
+  public subscription: Subscription;
   public profilePosts: ShowPostDTO[];
   public take = 5;
   public skip = 0;
@@ -18,11 +22,24 @@ export class ProfileGalleryComponent implements OnInit {
   constructor(
     private readonly route: ActivatedRoute,
     private readonly postsService: PostsService,
+    private readonly galleryRefresh: GalleryRefreshService,
+    private readonly notificationService: NotificationService,
   ) { }
 
   ngOnInit() {
-    this.route.data.subscribe(({ posts }) => this.profilePosts = posts);
-    console.log(this.route.snapshot.parent.params.id);
+
+
+    this.route.data.subscribe(({ posts }) => {
+      this.profilePosts = posts;
+      this.subscription = this.galleryRefresh.addPost$.subscribe(
+        (data: ShowPostDTO) => {
+          this.profilePosts.unshift(data);
+        });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   onScroll(): void {
@@ -39,6 +56,15 @@ export class ProfileGalleryComponent implements OnInit {
         }
       });
     }
+  }
+
+  public deletePost(postId: string): void {
+    this.postsService.deletePost(postId).subscribe(() => {
+      this.notificationService.success(`Post successfully deleted!`);
+    });
+
+    const index: number = this.profilePosts.findIndex(post => post.id === postId);
+    this.profilePosts.splice(index, 1);
   }
 
 }
