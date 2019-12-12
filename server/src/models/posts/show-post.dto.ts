3e83a6fc0b7b4773d97631d0add3abc7177fcfd3
Nikolay <nikolaytodor84@gmail.com>
2019-12-13 00:39:32 +0200

    transform dto for post info

--- server/src/models/posts/show-post.dto.ts
+++ server/src/models/posts/show-post.dto.ts
@@ -1,14 +1,36 @@
+import { Publish } from '../../transformer/decorators/publish';
+import { User } from '../../data/entities/user.entity';
+import { ShowAuthorDTO } from '../users/show-author-dto';
+
 export class ShowPostDTO {
+
+    @Publish()
     public id: string;
+
+    @Publish()
     public title: string;
+
+    @Publish()
     public content: string;
+
+    @Publish()
     public imageURL: string;
+
+    @Publish()
     public isPrivate: boolean;
+
+    @Publish()
     public dateCreated: string;
+
+    @Publish()
     public dateLastUpdated: string;
-    public author: string;
-    public authorUrl: string;
-    public authorId?: string;
+
+    @Publish(ShowAuthorDTO)
+    public author: User;
+
+    @Publish()
     public commentsCount: number;
-    public likes: number;
+
+    @Publish()
+    public likesCount: number;
 }
