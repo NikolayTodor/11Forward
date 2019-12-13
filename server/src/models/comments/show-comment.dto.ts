65a676622e901f12ba5a21aa0dd635d2ae82015d
Nikolay <nikolaytodor84@gmail.com>
2019-12-13 12:21:37 +0200

    implementing transform dto

--- server/src/models/comments/show-comment.dto.ts
+++ server/src/models/comments/show-comment.dto.ts
@@ -1,10 +1,23 @@
+import { Publish } from '../../transformer/decorators/publish';
+import { ShowAuthorDTO } from '../users/show-author-dto';
+import { User } from '../../data/entities/user.entity';
+
 export class ShowCommentDTO {
+    @Publish()
     public id: string;
+
+    @Publish()
     public content: string;
+
+    @Publish()
     public dateCreated: string;
+
+    @Publish()
     public dateLastUpdated: string;
-    public author: string;
-    public authorAvatar?: string;
-    public authorId?: string;
-    public likes: number;
+
+    @Publish(ShowAuthorDTO)
+    public author: User;
+
+    @Publish()
+    public likesCount: number;
 }
