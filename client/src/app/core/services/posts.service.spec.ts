04a157127277a60dd4105bf3f8453b420e16ceb9
Nikolay <nikolaytodor84@gmail.com>
2019-12-14 21:18:51 +0200

    implementing tests for post service

--- client/src/app/core/services/posts.service.spec.ts
+++ client/src/app/core/services/posts.service.spec.ts
@@ -29,3 +29,130 @@ describe('UserService', () => {
 
     service = TestBed.get(PostsService);
   }));
+
+  it('should be defined', () => {
+    // Arrange & Act & Assert
+    expect(service).toBeDefined();
+  });
+
+  describe('getPublicPosts', () => {
+    it('should call the http.get() method once with correct parameters', (done) => {
+      // Arrange
+      const mockTake = 5;
+      const mockSkip = 5;
+
+      const url = `${CONFIG.DOMAIN_NAME}/posts?take=${mockTake}&skip=${mockSkip}`;
+
+      const spy = jest
+        .spyOn(httpClient, 'get')
+        .mockReturnValue(of('mock'));
+
+
+      // Act & Assert
+      service.getPublicPosts(mockTake, mockSkip).subscribe(() => {
+        expect(spy).toBeCalledTimes(1);
+        expect(spy).toBeCalledWith(url);
+
+        done();
+      });
+    });
+
+    it('should return the http.get method return value', (done) => {
+
+      const mockTake = 5;
+      const mockSkip = 5;
+
+      const url = `${CONFIG.DOMAIN_NAME}/posts?take=${mockTake}&skip=${mockSkip}`;
+
+      const spy = jest
+        .spyOn(httpClient, 'get')
+        .mockReturnValue(of('mock'));
+
+
+      // Act & Assert
+      service.getPublicPosts(mockTake, mockSkip).subscribe((returnValue: any) => {
+        expect(returnValue).toBe('mock');
+        done();
+      });
+
+  });
+    describe('getAllPosts', () => {
+      it('should call the http.get() method once with correct parameters', (done) => {
+
+        const mockTake = 5;
+        const mockSkip = 5;
+
+        const url = `${CONFIG.DOMAIN_NAME}/posts/private?take=${mockTake}&skip=${mockSkip}`;
+
+        const spy = jest
+        .spyOn(httpClient, 'get')
+        .mockReturnValue(of('mock'));
+
+        service.getAllPosts(mockTake, mockSkip).subscribe(() => {
+          expect(spy).toBeCalledTimes(1);
+          expect(spy).toBeCalledWith(url);
+
+          done();
+        });
+      });
+
+      it('should return the http.get() method return value', (done) => {
+
+        const mockTake = 5;
+        const mockSkip = 5;
+
+        const url = `${CONFIG.DOMAIN_NAME}/posts/private?take=${mockTake}&skip=${mockSkip}`;
+
+        const spy = jest
+        .spyOn(httpClient, 'get')
+        .mockReturnValue(of('mock'));
+
+        service.getAllPosts(mockTake, mockSkip).subscribe((returnValue: any) => {
+          expect(returnValue).toBe('mock');
+          done();
+        });
+
+
+      });
+  });
+
+    describe('getPostById', () => {
+    it('should call the http.get() method once with correct parameters', (done) => {
+
+      const mockId = 'id1';
+      const mockValue = 'mock';
+      const url = `${CONFIG.DOMAIN_NAME}/posts/${mockId}`;
+
+      const spy = jest
+        .spyOn(httpClient, 'get')
+        .mockReturnValue(of(mockValue));
+
+      service.getPostById(mockId).subscribe(() => {
+        expect(spy).toBeCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith(url);
+
+        done();
+      });
+
+    });
+
+    it('should return the value from http.get()', (done)=>{
+      const mockId = 'id1';
+      const mockValue = 'mock';
+      const url = `${CONFIG.DOMAIN_NAME}/posts/${mockId}`;
+
+      const spy = jest
+        .spyOn(httpClient, 'get')
+        .mockReturnValue(of(mockValue));
+
+      service.getPostById(mockId).subscribe((returnValue: any) => {
+        expect(returnValue).toBe('mock');
+        done();
+      });
+    })
+
+  });
+
+});
+
+});
