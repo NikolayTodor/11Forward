36ad16e2f536bb59e9cf530d36e71fc9d934a169
Nikolay <nikolaytodor84@gmail.com>
2019-12-15 17:19:05 +0200

    setting swagger for comment creation

--- server/src/models/comments/create-comment.dto.ts
+++ server/src/models/comments/create-comment.dto.ts
@@ -2,7 +2,7 @@ import { ApiModelProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, Length } from 'class-validator';
 
 export class CreateCommentDTO {
-    @ApiModelProperty()
+    @ApiModelProperty({example: 'my new comment'})
     @IsString()
     @IsNotEmpty()
     @Length(5, 500)
