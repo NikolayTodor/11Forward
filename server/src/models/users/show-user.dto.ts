f9505fbd11bfeb3fb867756ff5c69a8256c46a6c
Nikolay <nikolaytodor84@gmail.com>
2019-12-15 14:30:06 +0200

    validation + swagger

--- server/src/models/users/show-user.dto.ts
+++ server/src/models/users/show-user.dto.ts
@@ -1,13 +1,24 @@
 import { Expose } from 'class-transformer';
+import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class ShowUserDTO {
 
+    @IsString()
+    @IsNotEmpty()
+    @ApiModelProperty({example: '2501'})
     @Expose()
     public id: string;
 
+    @IsString()
+    @IsNotEmpty()
+    @ApiModelProperty({example: 'niki'})
     @Expose()
     public username: string;
 
+    @IsString()
+    @IsNotEmpty()
+    @ApiModelProperty({example: 'niki@abv.bg'})
     @Expose()
     public email: string;
 
