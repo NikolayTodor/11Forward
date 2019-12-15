10e09c76999fb6f222c01fbcaba618e15e1c6d23
Nikolay <nikolaytodor84@gmail.com>
2019-12-15 18:48:57 +0200

    moving photo upload as an injectable helper fn

---
+++ server/src/common/services/photo-upload.service.ts
@@ -0,0 +1,24 @@
+import { ConfigService } from '../../config/config.service';
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class PhotoUploadService {
+    constructor(private readonly configService: ConfigService) {}
+
+    public async uploadPhoto(base: string): Promise<string> {
+
+        try {
+        const data = await axios(`https://api.imgur.com/3/upload`, {
+            method: 'POST',
+            headers: {
+               Authorization: this.configService.imgurClientId
+            },
+            data: {image: base},
+          });
+          return data.data.data.link;
+        } catch (error) {
+         console.log(error);
+        }
+    }
+}
