8cc476b555f52a6cd866af2a916132fb140a7568
Nikolay <nikolaytodor84@gmail.com>
2019-12-15 18:48:08 +0200

    module export photo upload service

--- server/src/common/core.module.ts
+++ server/src/common/core.module.ts
@@ -1,10 +1,13 @@
+
 import { Module, Global } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { ConfigModule } from '../config/config.module';
+import { PhotoUploadService } from './services/photo-upload.service';
 
 @Global()
 @Module({
   imports: [ConfigModule, AuthModule],
-  exports: [ConfigModule, AuthModule],
+  providers: [PhotoUploadService],
+  exports: [ConfigModule, AuthModule, PhotoUploadService],
 })
 export class CoreModule {}
