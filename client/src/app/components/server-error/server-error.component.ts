c64e20fb219fe673767a1509bfa3d4b255d587c5
Nikolay <nikolaytodor84@gmail.com>
2019-12-15 21:41:13 +0200

    removing unused ngOnInit

--- client/src/app/components/server-error/server-error.component.ts
+++ client/src/app/components/server-error/server-error.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-server-error',
   templateUrl: './server-error.component.html',
   styleUrls: ['./server-error.component.scss']
 })
-export class ServerErrorComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ServerErrorComponent {
+  constructor() {}
 }
